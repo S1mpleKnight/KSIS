d30364a120c090864f8c656564a293f78a64f9c4
S1mpleKnight <become.adult77@gmail.com>
2021-03-06 15:47:06 +0300

    server module started

--- second/server/src/by/bsuir/ksis/chat/server/Server.java
+++ second/server/src/by/bsuir/ksis/chat/server/Server.java
@@ -1,7 +1,48 @@
 package by.bsuir.ksis.chat.server;
 
-public class Server {
+import by.bsuir.ksis.connection.Connection;
+import by.bsuir.ksis.connection.ConnectionActions;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Server  implements ConnectionActions {
     public static void main(String[] args) {
+
+    }
+
+    private Server(){
+        System.out.println("Server running");
+        try (ServerSocket serverSocket = new ServerSocket(8189)){
+            while (true){
+                try {
+                    new Connection();
+                } catch (){
+
+                }
+            }
+        } catch (IOException e) {
+
+        }
+    }
+
+    @Override
+    public void connectionReady(Connection connection) {
+
+    }
+
+    @Override
+    public void stringReceived(Connection connection, String message) {
+
+    }
+
+    @Override
+    public void disconnect(Connection connection) {
+
+    }
+
+    @Override
+    public void exception(Connection connection, Exception e) {
 
     }
 }
