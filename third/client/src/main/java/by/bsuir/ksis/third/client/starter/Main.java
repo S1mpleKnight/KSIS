f097cb3a3dc4cc56d8361bf29c5355405f0d6bc6
S1mpleKnight <become.adult77@gmail.com>
2021-05-26 14:05:34 +0300

    init client

---
+++ third/client/src/main/java/by/bsuir/ksis/third/client/starter/Main.java
@@ -0,0 +1,16 @@
+package by.bsuir.ksis.third.client.starter;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
