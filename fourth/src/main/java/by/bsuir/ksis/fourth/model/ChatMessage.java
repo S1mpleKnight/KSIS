9fc532cfabf3750306eb40a94c9950b0dddea956
S1mpleKnight <become.adult77@gmail.com>
2021-06-01 17:04:10 +0300

    chat without auth done

---
+++ fourth/src/main/java/by/bsuir/ksis/fourth/model/ChatMessage.java
@@ -0,0 +1,18 @@
+package by.bsuir.ksis.fourth.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
